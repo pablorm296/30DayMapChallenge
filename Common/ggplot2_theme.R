9d8e2a818c79e7d3e7d4e68ae05f7aaeb3f81090
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-11-02 22:22:24 -0600

    Reduced margins in default black theme and added 'reduced margins' theme

--- Common/ggplot2_theme.R
+++ Common/ggplot2_theme.R
@@ -5,13 +5,36 @@ theme_30DayMapChallenge_black <- function(...) {
             text = element_text(family = "Montserrat", colour = "#ffffff"),
             title = element_text(family = "Montserrat", colour = "#ffffff"),
             plot.subtitle = element_text(size = 16, hjust = 0.5,
-                                         margin = margin(t = 14, b = 16, unit = "pt")),
+                                         margin = margin(t = 10, b = 10, unit = "pt")),
             plot.title = element_text(hjust = 0.5, size = 20, family = "Keep Calm Med",
-                                      margin = margin(t = 20, b = 20, unit = "pt")),
+                                      margin = margin(t = 10, b = 10, unit = "pt")),
             legend.title = element_text(size = 14),
             axis.text = element_blank(),
             axis.title = element_blank(),
-            plot.margin = unit(c(1, 1, 1, 1), units = "cm"),
+            plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), units = "cm"),
+            plot.background = element_rect(fill = "#2d2d2d", colour = NA),
+            panel.background = element_rect(fill = "#2d2d2d", colour = NA),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.caption = element_text(size = 9, lineheight = 1, hjust = 1),
+            legend.position = "bottom",
+            ...
+        )
+}
+
+theme_30DayMapChallenge_black_reduced <- function(...) {
+    theme_minimal() %+replace% 
+        theme(
+            text = element_text(family = "Montserrat", colour = "#ffffff"),
+            title = element_text(family = "Montserrat", colour = "#ffffff"),
+            plot.subtitle = element_text(size = 16, hjust = 0.5,
+                                         margin = margin(t = 5, b = 5, unit = "pt")),
+            plot.title = element_text(hjust = 0.5, size = 20, family = "Keep Calm Med",
+                                      margin = margin(t = 5, b = 5, unit = "pt")),
+            legend.title = element_text(size = 14),
+            axis.text = element_blank(),
+            axis.title = element_blank(),
+            plot.margin = unit(c(0.1, 0.1, 0.1, 0.1), units = "cm"),
             plot.background = element_rect(fill = "#2d2d2d", colour = NA),
             panel.background = element_rect(fill = "#2d2d2d", colour = NA),
             panel.grid.major = element_blank(),
