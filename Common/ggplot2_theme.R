4b239265f05be91beed21df1edda8eff1c28dbcf
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-11-20 13:31:32 -0600

    Added no margins white theme

--- Common/ggplot2_theme.R
+++ Common/ggplot2_theme.R
@@ -90,3 +90,49 @@ theme_30DayMapChallenge_white <- function(...) {
             ...
         )
 }
+
+theme_30DayMapChallenge_white_reduced <- function(...) {
+    theme_minimal() %+replace% 
+        theme(
+            text = element_text(family = "Montserrat", colour = "#404040"),
+            title = element_text(family = "Montserrat", colour = "#404040"),
+            plot.subtitle = element_text(size = 16, hjust = 0.5,
+                                         margin = margin(t = 5, b = 5, unit = "pt")),
+            plot.title = element_text(hjust = 0.5, size = 20, family = "Keep Calm Med",
+                                      margin = margin(t = 5, b = 5, unit = "pt")),
+            legend.title = element_text(size = 14),
+            axis.text = element_blank(),
+            axis.title = element_blank(),
+            plot.margin = unit(c(0.1, 0.1, 0.1, 0.1), units = "cm"),
+            plot.background = element_rect(fill = "#FFFFFF", colour = NA),
+            panel.background = element_rect(fill = "#FFFFFF", colour = NA),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.caption = element_text(size = 9, lineheight = 1, hjust = 1),
+            legend.position = "bottom",
+            ...
+        )
+}
+
+theme_30DayMapChallenge_whiter_ultrareduced <- function(...) {
+    theme_minimal() %+replace% 
+        theme(
+            text = element_text(family = "Montserrat", colour = "#808080"),
+            title = element_text(family = "Montserrat", colour = "#808080"),
+            plot.subtitle = element_text(size = 16, hjust = 0.5,
+                                         margin = margin(t = 5, b = 5, unit = "pt")),
+            plot.title = element_text(hjust = 0.5, size = 20, family = "Keep Calm Med",
+                                      margin = margin(t = 5, b = 5, unit = "pt")),
+            legend.title = element_text(size = 14),
+            axis.text = element_blank(),
+            axis.title = element_blank(),
+            plot.margin = unit(c(0, 0, 0, 0), units = "cm"),
+            plot.background = element_rect(fill = "#FFFFFF", colour = NA),
+            panel.background = element_rect(fill = "#FFFFFF", colour = NA),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.caption = element_text(size = 9, lineheight = 1, hjust = 1),
+            legend.position = "bottom",
+            ...
+        )
+}
