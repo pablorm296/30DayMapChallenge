136ce482921f7d86c8c7cb30c985ff789703c5f7
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-11-20 09:50:13 -0600

    Added white theme to common ggplot2 themes

--- Common/ggplot2_theme.R
+++ Common/ggplot2_theme.R
@@ -67,3 +67,26 @@ theme_30DayMapChallenge_clear <- function(...) {
             ...
         )
 }
+
+theme_30DayMapChallenge_white <- function(...) {
+    theme_minimal() %+replace% 
+        theme(
+            text = element_text(family = "Montserrat", colour = "#404040"),
+            title = element_text(family = "Montserrat", colour = "#404040"),
+            plot.subtitle = element_text(size = 16, hjust = 0.5,
+                                         margin = margin(t = 10, b = 10, unit = "pt")),
+            plot.title = element_text(hjust = 0.5, size = 20, family = "Keep Calm Med",
+                                      margin = margin(t = 10, b = 10, unit = "pt")),
+            legend.title = element_text(size = 14),
+            axis.text = element_blank(),
+            axis.title = element_blank(),
+            plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), units = "cm"),
+            plot.background = element_rect(fill = "#FFFFFF", colour = NA),
+            panel.background = element_rect(fill = "#FFFFFF", colour = NA),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.caption = element_text(size = 9, lineheight = 1, hjust = 1),
+            legend.position = "bottom",
+            ...
+        )
+}
